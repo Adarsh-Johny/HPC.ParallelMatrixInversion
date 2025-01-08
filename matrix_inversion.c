beeb22408efbd930598fa40173711f4dff84797e
Susanna Repo <susanna.repo@hpc-head-n2.unitn.it>
2025-01-08 12:25:07 +0100

    Re-indent code

--- matrix_inversion.c
+++ matrix_inversion.c
@@ -24,358 +24,358 @@ void copy_mat(int nrow, int ncol, double src[nrow][ncol], double dest[nrow][ncol
 
 
 int main(int argc, char* argv[]) {
-	/* int n = 3; */ /* Size of the matrix */
-	
-	/* Get and print input matrix */
-	/*
-	double mat[3][3] = { {0, 3, 2}, {1, 0, 2}, {0, 0, 1} };
-	int nrow = sizeof(mat) / sizeof(mat[0]);
-	int ncol = sizeof(mat[0]) / sizeof(mat[0][0]);
+    /* int n = 3; */ /* Size of the matrix */
 
- 	print_mat(nrow, ncol, mat);
-	*/
+    /* Get and print input matrix */
+    /*
+       double mat[3][3] = { {0, 3, 2}, {1, 0, 2}, {0, 0, 1} };
+       int nrow = sizeof(mat) / sizeof(mat[0]);
+       int ncol = sizeof(mat[0]) / sizeof(mat[0][0]);
 
-	
-	FILE *fp = fopen("HPC.ParallelMatrixInversion/test_matrices/mat_3x3_i_1.txt", "r");
-	/* FILE *fp = fopen("test_matrices.txt", "r"); */
-	/* Check that the file exists */
-	if (!fp) {
-		perror("fopen");
-		return 1;
-	}	
-
-	const char *directory_path = "HPC.ParallelMatrixInversion/test_matrices/";
-        DIR *dir = opendir(directory_path);
-	if (!dir) {
-		perror("opendir");
-		return 1;
-	}
-
-	printf("Opening dir %s successfull\n", directory_path);
+       print_mat(nrow, ncol, mat);
+       */
 
 
-	struct dirent *entry;
-	while ((entry = readdir(dir)) != NULL) {
+    FILE *fp = fopen("HPC.ParallelMatrixInversion/test_matrices/mat_3x3_i_1.txt", "r");
+    /* FILE *fp = fopen("test_matrices.txt", "r"); */
+    /* Check that the file exists */
+    if (!fp) {
+	perror("fopen");
+	return 1;
+    }	
+
+    const char *directory_path = "HPC.ParallelMatrixInversion/test_matrices/";
+    DIR *dir = opendir(directory_path);
+    if (!dir) {
+	perror("opendir");
+	return 1;
+    }
+
+    printf("Opening dir %s successfull\n", directory_path);
+
+
+    struct dirent *entry;
+    while ((entry = readdir(dir)) != NULL) {
 	/* Skip directories (there shouldn't be any) */
-		if (entry->d_type == DT_REG) {
-			/* process_file(entry->d_name); */
-			/* printf("file name %s", entry->d_name); */
-			invert_mat_from_file(directory_path, entry->d_name);
-		}
+	if (entry->d_type == DT_REG) {
+	    /* process_file(entry->d_name); */
+	    /* printf("file name %s", entry->d_name); */
+	    invert_mat_from_file(directory_path, entry->d_name);
 	}
-	closedir(dir);
+    }
+    closedir(dir);
 
-	return 0;
+    return 0;
 }
 
 void print_working_dir() {	
-	/* Check the working directory */
-	char cwd[1024];
-	if (getcwd(cwd, sizeof(cwd)) != NULL) {
-		printf("Current working directory: %s\n", cwd);
-	} else {
-		perror("getcwd");
-	}
+    /* Check the working directory */
+    char cwd[1024];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+	printf("Current working directory: %s\n", cwd);
+    } else {
+	perror("getcwd");
+    }
 }
 
 bool check_inverse(int nrow, int ncol, double m1[nrow][ncol], double m2[nrow][ncol]) {
-	bool success = true;
-	double mat_res[nrow][ncol];
-	int i, j, k;
+    bool success = true;
+    double mat_res[nrow][ncol];
+    int i, j, k;
 
-	for (i = 0; i < nrow; i++) {
-		for (j = 0; j < ncol; j++) {
-			mat_res[i][j] = 0;
+    for (i = 0; i < nrow; i++) {
+	for (j = 0; j < ncol; j++) {
+	    mat_res[i][j] = 0;
 
-			for (k = 0; k < nrow; k++) {
-				mat_res[i][j] += m1[i][k] * m2[k][j];
-			}
-			
-			/* Check if the result is identity (will fail if there are nans or +-inf) */
-			if (i == j && fabs(1 - mat_res[i][j]) > 1e-9) {
-				success = false;
-			}
-			
-			if (i != j && fabs(mat_res[i][j]) > 1e-9) {
-				success = false;
-			}
-		}
+	    for (k = 0; k < nrow; k++) {
+		mat_res[i][j] += m1[i][k] * m2[k][j];
+	    }
+
+	    /* Check if the result is identity (will fail if there are nans or +-inf) */
+	    if (i == j && fabs(1 - mat_res[i][j]) > 1e-9) {
+		success = false;
+	    }
+
+	    if (i != j && fabs(mat_res[i][j]) > 1e-9) {
+		success = false;
+	    }
 	}
+    }
 
-	return success;
+    return success;
 }
 
 /* A method to run test matrices on invert_matrix */
 bool invert_mat_from_file(const char* dir, const char* fname) {
-	/* Create the target file path  */
-	char* full_path = malloc(strlen(dir) + strlen(fname) + 1);
+    /* Create the target file path  */
+    char* full_path = malloc(strlen(dir) + strlen(fname) + 1);
 
-	if (!full_path) {
-		perror("malloc (concatenate file path)");
+    if (!full_path) {
+	perror("malloc (concatenate file path)");
+	return false;
+    }
+
+    strcpy(full_path, dir);
+    strcat(full_path, fname);
+
+    /* Extract dimensions and type of matrix (invertible or singular) from the filename */
+    printf("Reading matrix from file %s\n", fname);
+    int nrow, ncol;
+    char kind;
+
+    if (sscanf(fname, "mat_%dx%d_%c", &nrow, &ncol, &kind) != 3) {
+	printf("Invalid filename format: %s\n", fname);
+	return false;
+    }
+
+    /* Open the file */
+    printf("Trying to read %dx%d matrix from %s ", nrow, ncol, full_path);
+    printf("(%s)\n", (kind == 'i') ? "invertible" : "singular");
+
+    FILE *fp = fopen(full_path, "r");
+    if (!fp) {
+	perror("Error opening file");
+	print_working_dir();
+	return false;
+    }
+
+    /* Scan the file and read the matrix */
+    int i, j;
+    double mat[nrow][ncol];
+
+    for (i = 0; i < nrow; ++i) {
+	for (j = 0; j < ncol; ++j) {
+	    if (fscanf(fp, "%lf", &mat[i][j]) != 1) {
+		printf("Error reading matrix value at [%d][%d] in file: %s\n", i, j, fname);
+		fclose(fp);
 		return false;
+	    }
 	}
+    }
 
-	strcpy(full_path, dir);
-	strcat(full_path, fname);
-	
-	/* Extract dimensions and type of matrix (invertible or singular) from the filename */
-	printf("Reading matrix from file %s\n", fname);
-	int nrow, ncol;
-	char kind;
+    fclose(fp);
+    free(full_path);
 
-	if (sscanf(fname, "mat_%dx%d_%c", &nrow, &ncol, &kind) != 3) {
-		printf("Invalid filename format: %s\n", fname);
-		return false;
+    /* Create a copy of the input matrix before inverting it */
+    double mat_cp[nrow][ncol];
+    copy_mat(nrow, ncol, mat, mat_cp);
+
+    /* Invert the matrix */
+    double mat_inv[nrow][ncol];
+    bool res = invert_matrix(nrow, ncol, mat_cp, mat_inv);
+
+    if (res && kind == 'i') {
+	if (check_inverse(nrow, ncol, mat, mat_inv)) {
+	    printf("Found correct inverse\n");
+	} else {
+	    printf("ERROR: Didn't find inverse for invertible matrix\n");
 	}
+    } else if (!res && kind == 'i') {
+	printf("ERROR: Failed to invert invertible matrix\n");
+    }
 
-	/* Open the file */
-	printf("Trying to read %dx%d matrix from %s ", nrow, ncol, full_path);
-	printf("(%s)\n", (kind == 'i') ? "invertible" : "singular");
-	
-	FILE *fp = fopen(full_path, "r");
-	if (!fp) {
-		perror("Error opening file");
-		print_working_dir();
-		return false;
-	}
+    printf("\n\n");
 
-	/* Scan the file and read the matrix */
-	int i, j;
-	double mat[nrow][ncol];
-
-	for (i = 0; i < nrow; ++i) {
-		for (j = 0; j < ncol; ++j) {
-			if (fscanf(fp, "%lf", &mat[i][j]) != 1) {
-				printf("Error reading matrix value at [%d][%d] in file: %s\n", i, j, fname);
-				fclose(fp);
-				return false;
-			}
-		}
-	}
-
-	fclose(fp);
-	free(full_path);
-
-	/* Create a copy of the input matrix before inverting it */
-	double mat_cp[nrow][ncol];
-	copy_mat(nrow, ncol, mat, mat_cp);
-
-	/* Invert the matrix */
-	double mat_inv[nrow][ncol];
-	bool res = invert_matrix(nrow, ncol, mat_cp, mat_inv);
-
-	if (res && kind == 'i') {
-	        if (check_inverse(nrow, ncol, mat, mat_inv)) {
-		    	printf("Found correct inverse\n");
-	        } else {
-			printf("ERROR: Didn't find inverse for invertible matrix\n");
-	        }
-	} else if (!res && kind == 'i') {
-	        printf("ERROR: Failed to invert invertible matrix\n");
-	}
-
-	printf("\n\n");
-
-	return true;
+    return true;
 }
 
 bool invert_matrix(int nrow, int ncol, double mat[nrow][ncol], double mat_inv[nrow][ncol]) {
-	int n = nrow;
+    int n = nrow;
 
-	/* Augment identity */
-	double mat_aug[n][2 * n];
-	augment_mat(n, mat, mat_aug);
+    /* Augment identity */
+    double mat_aug[n][2 * n];
+    augment_mat(n, mat, mat_aug);
 
-	/* Forward elimination */
-	bool res = gaussian_elimination(n, 2 * n, mat_aug);
-	if (res) {
-		printf("GE successsful\n");
-	} else {
-		printf("GE failed\n");
-		return false;
-	}
+    /* Forward elimination */
+    bool res = gaussian_elimination(n, 2 * n, mat_aug);
+    if (res) {
+	printf("GE successsful\n");
+    } else {
+	printf("GE failed\n");
+	return false;
+    }
 
-	printf("Matrix after gaussian elimination (should have non-zero diagonal)\n");
-	print_mat(n, 2 * n, mat_aug);
+    printf("Matrix after gaussian elimination (should have non-zero diagonal)\n");
+    print_mat(n, 2 * n, mat_aug);
 
-	
-	/* Backward elimination */
-	bool res2 = rref(n, 2 * n, mat_aug);
-	if (res2) {
-		printf("RREF successful\n");
-	} else {
-		printf("RREF failed\n");
-		return false;
-	}
 
-	printf("Matrix after RREF\n");
-	print_mat(n, 2 * n, mat_aug);
+    /* Backward elimination */
+    bool res2 = rref(n, 2 * n, mat_aug);
+    if (res2) {
+	printf("RREF successful\n");
+    } else {
+	printf("RREF failed\n");
+	return false;
+    }
 
-	/* Extract inverse if it the steps before were successfull */
-	/* double mat_inv[n][n]; */
-	extract_inverse(n, 2 * n, mat_aug, mat_inv);
+    printf("Matrix after RREF\n");
+    print_mat(n, 2 * n, mat_aug);
 
-	printf("The extracted inverse matrix\n");
-	print_mat(n, n, mat_inv);
-	
-	return true;
+    /* Extract inverse if it the steps before were successfull */
+    /* double mat_inv[n][n]; */
+    extract_inverse(n, 2 * n, mat_aug, mat_inv);
+
+    printf("The extracted inverse matrix\n");
+    print_mat(n, n, mat_inv);
+
+    return true;
 }
 
 /* Extract the augmented part of the augmented matrix */
 void extract_inverse(int nrow, int ncol, double mat_aug[nrow][ncol], double mat_inv[nrow][nrow]) {
-	int i, j;
+    int i, j;
 
-	for (i = 0; i < nrow; i++) {
-		for (j = 0; j < ncol; j++) {
-			/* Copy only the right side of the augmented matrix */
-			mat_inv[i][j] = mat_aug[i][nrow + j];
-		}
+    for (i = 0; i < nrow; i++) {
+	for (j = 0; j < ncol; j++) {
+	    /* Copy only the right side of the augmented matrix */
+	    mat_inv[i][j] = mat_aug[i][nrow + j];
 	}
+    }
 }
 
 /* Second part of the Gauss-Jordan elimination, results in the reduced row echelon form */
 bool rref(int nrow, int ncol, double mat[nrow][ncol]) {
-	int i; 
-	/* printf("rref input: nrow = %d, ncol = %d, matrix = \n", nrow, ncol); */
-	/* print_mat(nrow, ncol, mat); */
+    int i; 
+    /* printf("rref input: nrow = %d, ncol = %d, matrix = \n", nrow, ncol); */
+    /* print_mat(nrow, ncol, mat); */
 
-	for (i = nrow - 1; i > 0; i--) {
-		/* printf("GE: Eliminating the rows above %d\n", i); */
-		int r;
-		for (r = i-1; r >= 0; r--) {
-			/* printf("GE: Eliminating row %d\n", r); */
-			double coeff = mat[r][i];
-			subtract_row(i, r, coeff, nrow, ncol, mat);
-		}
+    for (i = nrow - 1; i > 0; i--) {
+	/* printf("GE: Eliminating the rows above %d\n", i); */
+	int r;
+	for (r = i-1; r >= 0; r--) {
+	    /* printf("GE: Eliminating row %d\n", r); */
+	    double coeff = mat[r][i];
+	    subtract_row(i, r, coeff, nrow, ncol, mat);
 	}
-	return true;
+    }
+    return true;
 }
 
 
 /* Normalize pivots and clear nonzero values below diagonal */
 bool gaussian_elimination(int nrow, int ncol, double mat[nrow][ncol]) {
-	int i, r;
+    int i, r;
 
-	/* Iterate the rows of mat */
-	for (i = 0; i < nrow; i++) {
-		if (fabs(mat[i][i]) < 1e-9) {
+    /* Iterate the rows of mat */
+    for (i = 0; i < nrow; i++) {
+	if (fabs(mat[i][i]) < 1e-9) {
 
 
-			/* Find row below the current row where the current column index is nonzero */
-			bool found = false;
-			for (r = i+1; r < nrow; r++) {
-				if (fabs(mat[r][i]) > 1e-9) {
-					swap_rows(i, r, nrow, ncol, mat);
-					found = true;
-					break;
-				}
-			}
-			if (!found) {
-				printf("Matrix is singular or nearly singular\n");
-				return false;
-			}
-
+	    /* Find row below the current row where the current column index is nonzero */
+	    bool found = false;
+	    for (r = i+1; r < nrow; r++) {
+		if (fabs(mat[r][i]) > 1e-9) {
+		    swap_rows(i, r, nrow, ncol, mat);
+		    found = true;
+		    break;
 		}
+	    }
+	    if (!found) {
+		printf("Matrix is singular or nearly singular\n");
+		return false;
+	    }
 
-		/* Normalize the row */
-		double s = 1/mat[i][i];
-		multiply_row(i, s, nrow, ncol, mat);
-
-		/* Eliminate nonzero values below */
-		int r;
-		for (r = i+1; r < nrow; r++) {
-			double coeff = mat[r][i];
-			subtract_row(i, r, coeff, nrow, ncol, mat);
-		}
 	}
 
-	printf("Gaussian elimination done (upper triangular matrix wiht nonzero diagonal)\n");
-	return true;
+	/* Normalize the row */
+	double s = 1/mat[i][i];
+	multiply_row(i, s, nrow, ncol, mat);
+
+	/* Eliminate nonzero values below */
+	int r;
+	for (r = i+1; r < nrow; r++) {
+	    double coeff = mat[r][i];
+	    subtract_row(i, r, coeff, nrow, ncol, mat);
+	}
+    }
+
+    printf("Gaussian elimination done (upper triangular matrix wiht nonzero diagonal)\n");
+    return true;
 }
 
 /* Subtract the values of row row_idx multiplied with coefficient coeff from the row given by target_idx */
 void subtract_row(int row_idx, int target_idx, double coeff, int nrow, int ncol, double mat[nrow][ncol]) {
-	/* TODO: debug printing */
+    /* TODO: debug printing */
 
-	if (row_idx < 0 || row_idx >= nrow) {
-		printf("Subtract row: invalid row index %d for matrix %d x %d\n", row_idx, nrow, ncol);
-	}
-	
-	if (target_idx < 0 || target_idx >= nrow) {
-		printf("Subtract row: invalid target row index %d for matrix %d x %d\n", target_idx, nrow, ncol);
-	}
+    if (row_idx < 0 || row_idx >= nrow) {
+	printf("Subtract row: invalid row index %d for matrix %d x %d\n", row_idx, nrow, ncol);
+    }
 
-	int i;
-	for (i = 0; i < ncol; i++) {
-		mat[target_idx][i] -= mat[row_idx][i] * coeff;
-	}
+    if (target_idx < 0 || target_idx >= nrow) {
+	printf("Subtract row: invalid target row index %d for matrix %d x %d\n", target_idx, nrow, ncol);
+    }
+
+    int i;
+    for (i = 0; i < ncol; i++) {
+	mat[target_idx][i] -= mat[row_idx][i] * coeff;
+    }
 }
 
 void multiply_row(int row_idx, double s, int nrow, int ncol, double mat[nrow][ncol]) {	
-	/* TODO: remove later, random debugging */
-	if (row_idx < 0 || row_idx >= nrow) {
-		printf("Multiply row: invalid row index %d for matrix %d x %d", row_idx, nrow, ncol);
-		return;
-	}
+    /* TODO: remove later, random debugging */
+    if (row_idx < 0 || row_idx >= nrow) {
+	printf("Multiply row: invalid row index %d for matrix %d x %d", row_idx, nrow, ncol);
+	return;
+    }
 
-	int j;
-	for (j = 0; j < ncol; j++) {
-		mat[row_idx][j] *= s;
-	}
+    int j;
+    for (j = 0; j < ncol; j++) {
+	mat[row_idx][j] *= s;
+    }
 }
 
 
 void swap_rows(int r1, int r2, int nrow, int ncol, double mat[nrow][ncol]) {
-	/* TODO: remove later, random debugging */
-	if (r1 < 0 || r1 >= nrow) {
-		printf("Swap row: invalid row index %d for %d x %d matrix", r1, nrow, ncol);
-	}
-	if (r2 < 0 || r2 >= nrow) {
-		printf("Swap row: invalid row index %d for %d x %d matrix", r2, nrow, ncol);
-	}
+    /* TODO: remove later, random debugging */
+    if (r1 < 0 || r1 >= nrow) {
+	printf("Swap row: invalid row index %d for %d x %d matrix", r1, nrow, ncol);
+    }
+    if (r2 < 0 || r2 >= nrow) {
+	printf("Swap row: invalid row index %d for %d x %d matrix", r2, nrow, ncol);
+    }
 
-	double temp;
-	int i;
-	for (i = 0; i < ncol; i++) {
-		temp = mat[r1][i];
-		mat[r1][i] = mat[r2][i];
-		mat[r2][i] = temp;
-	}
+    double temp;
+    int i;
+    for (i = 0; i < ncol; i++) {
+	temp = mat[r1][i];
+	mat[r1][i] = mat[r2][i];
+	mat[r2][i] = temp;
+    }
 }
 
 /* This surely can be parallelized */
 void augment_mat(int n, double mat[n][n], double mat_aug[n][2 * n]) {
-	int row, col;
+    int row, col;
 
-	/* Parallelize the copying of the rows */
-	for (row = 0; row < n; row++) {
-		for (col = 0; col < n; col++) {
-			/* Copy the row of original matrix */
-			mat_aug[row][col] = mat[row][col];
-			/* Create a row of identity matrix to the right */
-			mat_aug[row][n + col] = (row == col) ? 1 : 0;
-		}
+    /* Parallelize the copying of the rows */
+    for (row = 0; row < n; row++) {
+	for (col = 0; col < n; col++) {
+	    /* Copy the row of original matrix */
+	    mat_aug[row][col] = mat[row][col];
+	    /* Create a row of identity matrix to the right */
+	    mat_aug[row][n + col] = (row == col) ? 1 : 0;
 	}
+    }
 }
 
 void print_mat(int nrow, int ncol, double mat[nrow][ncol]) {
-	int i, j;
+    int i, j;
 
-	for (i = 0; i < nrow; i++) {
-		for (j = 0; j < ncol; j++) {
-			printf("%.2f \t", mat[i][j]);
-		}
-		printf("\n");
+    for (i = 0; i < nrow; i++) {
+	for (j = 0; j < ncol; j++) {
+	    printf("%.2f \t", mat[i][j]);
 	}
+	printf("\n");
+    }
 }
 
 void copy_mat(int nrow, int ncol, double src[nrow][ncol], double dest[nrow][ncol]) {
-	int i, j;
-	for (i = 0; i < nrow; i++) {
-		for (j = 0; j < ncol; j++) {
-			dest[i][j] = src[i][j];
-		}
+    int i, j;
+    for (i = 0; i < nrow; i++) {
+	for (j = 0; j < ncol; j++) {
+	    dest[i][j] = src[i][j];
 	}
+    }
 }
 
 
