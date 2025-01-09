1ee030fbb121f7b6500cbf2706ec37e353325bdc
Susanna Repo <susanna.repo@hpc-head-n1.unitn.it>
2025-01-09 19:01:54 +0100

    Add example bash file

---
+++ matrix_inversion.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#PBS -l select=2:ncpus=10:mem=2gb
+#PBS -l walltime=0:01:00
+#PBS -q short_cpuQ
+
+# load the module
+module load openmpi-4.0.4
+
+# Actual running command
+./HPC.ParallelMatrixInversion/matrix_inversion
+
